62ea246ca49bfad1adb704817d2abff7b70823d9
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-09-06 09:05:57 +0200

    Fix and switch improper class names

---
+++ ComunicazioneFattureCorrispettivi/Annullamento/Annullamento.cs
@@ -0,0 +1,6 @@
+﻿using ComunicazioneFattureCorrispettivi.Common;
+
+namespace ComunicazioneFattureCorrispettivi.Annullamento
+{
+    public class Annullamento : Rettifica { }
+}
--- ComunicazioneFattureCorrispettivi/FattureEmesse/FattureEmesse.cs
+++ ComunicazioneFattureCorrispettivi/FattureEmesse/FattureEmesse.cs
@@ -1,6 +1,32 @@
-﻿using ComunicazioneFattureCorrispettivi.Common;
+﻿using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using ComunicazioneFattureCorrispettivi.Common;
+using FatturaElettronica.Common;
 
-namespace ComunicazioneFattureCorrispettivi.Annullamento
+namespace ComunicazioneFattureCorrispettivi.FattureEmesse
 {
-    public class Annullamento : Rettifica { }
+    public class FattureEmesse : BaseClassSerializable
+    {
+        private readonly CedentePrestatore _cedentePrestatore;
+        private readonly List<CessionarioCommittente> _cessionarioCommittente;
+        private readonly Rettifica _rettifica;
+
+        public FattureEmesse()
+        {
+            _cedentePrestatore = new CedentePrestatore();
+            _cessionarioCommittente = new List<CessionarioCommittente>();
+            _rettifica = new Rettifica();
+        }
+        public FattureEmesse(XmlReader r) : base(r) { }
+
+        [DataProperty]
+        [XmlElement(ElementName = "CedentePrestatoreDTE")]
+        public CedentePrestatore CedentePrestatore => _cedentePrestatore;
+        [DataProperty]
+        [XmlElement(ElementName = "CessionarioCommittenteDTE")]
+        public List<CessionarioCommittente> CessionarioCommittente => _cessionarioCommittente;
+        [DataProperty]
+        public Rettifica Rettifica => _rettifica;
+    }
 }
